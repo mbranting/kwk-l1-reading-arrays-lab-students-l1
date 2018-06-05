4a0ec93ce23744e82512907d57b13f2aef35605c
mbranting <mckenna.branting@gmail.com>
2018-06-05 21:52:35 +0000

    automatically backed up by learn

---
+++ arrays.rb
@@ -0,0 +1,25 @@
+# # student_names=[]
+# # #arrays are formed by using square brackets [ ]
+
+# student_names = ['Sam', 'Jess', 'Ash', 'Carla']
+# # #puts student_names 
+
+# # #puts [name of your array] prints out the information in your array in the terminal
+
+# # #puts student_names[0]
+
+# # #position zero 0 gives you the first value in the array
+
+# # student_names.push('Cardi')
+# # #puts student_names
+
+# # #array name ('what you want to add').push will add it to the array
+
+# # student_names.push('Calabria')
+# # student_names.push ('Noah')
+# # student_names.push ("Jasmine")
+# # puts student_names
+
+# student_names << ('mac')
+# puts student_names
+
